885c81a525ad11f0816f5f761d4e3b652691b0eb
m-dkhan <mdzaidkhan628@gmail.com>
2025-03-29 20:24:57 -0400

    Added Seller Dashboard

--- obj/Debug/net8.0/RealEstateManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RealEstateManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstateManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fd748dec52aaa9dfd6f5551bfa9314315120f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3be6b7ba34fcc5fcfb8609b5d13630968f68a4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstateManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstateManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
